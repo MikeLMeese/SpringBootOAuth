203ea2d42cd50b73d1c45d34b3f37ce821ecc17d
MikeLMeese <101145367+MikeLMeese@users.noreply.github.com>
2022-06-03 08:44:18 -0400

    Added files

---
+++ src/test/java/win/spring/springbootoauth/SpringBootOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package win.spring.springbootoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
